f1962c061217725d4c5971da78db0e51644e9c60
Gustavo Gonzalez <gustavoagg@gmail.com>
2017-03-19 20:54:57 -0400

    Base Maven Project using Spring-boot with JDBC, JPA, Web and Mysql

---
+++ LoginSuggest/src/main/java/com/intertecintl/LoginSuggestApplication.java
@@ -0,0 +1,12 @@
+package com.intertecintl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSuggestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSuggestApplication.class, args);
+	}
+}
