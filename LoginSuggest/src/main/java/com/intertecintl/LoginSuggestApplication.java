159f996a4a68f148704a29ae812d2fc1ee6b6acb
Gustavo Gonzalez <gustavoagg@gmail.com>
2017-03-19 23:46:00 -0400

    Adding missing configuration - Running on IDE

--- LoginSuggest/src/main/java/com/intertecintl/LoginSuggestApplication.java
+++ LoginSuggest/src/main/java/com/intertecintl/LoginSuggestApplication.java
@@ -2,11 +2,19 @@ package com.intertecintl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginSuggestApplication {
+public class LoginSuggestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoginSuggestApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(LoginSuggestApplication.class);
+
 	}
 }
