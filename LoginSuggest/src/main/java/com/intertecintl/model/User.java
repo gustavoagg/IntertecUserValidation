8ad75e9be6afd9243aaeef21259f77f90d295c11
Gustavo Gonzalez <gustavoagg@gmail.com>
2017-03-19 21:42:53 -0400

    Model Definition: User Entity

---
+++ LoginSuggest/src/main/java/com/intertecintl/model/User.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.intertecintl.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	long id;
+	
+	String username;
+
+	public User(long id, String username) {
+		super();
+		this.id = id;
+		this.username = username;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
